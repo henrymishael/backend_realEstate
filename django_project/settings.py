ca76908272a428cca3b4bcfc3099ebf97be28f9b
henrymishael <henrymishael.hm@gmail.com>
2023-10-13 13:45:34 +0100

    gunicorn commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = "django-insecure-hs(pnmunk!k-bt(apw$=t$p4j5qy@!%r)1yha$q6)tb84kfxdh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", default=False)
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [".your-render-app.onrender.com",".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
